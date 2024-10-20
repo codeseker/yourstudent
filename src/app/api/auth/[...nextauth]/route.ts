4f2f0b4d4f343228c7da287bb0e891f1a588e2da
Ayush Jangid <ayushjangid441@gmail.com>
2024-10-20 22:47:17 +0530

    ts errors fixed

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -8,6 +8,11 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET, // Add this line
+  pages: {
+    signIn: "/auth/signin", // Custom sign-in page if needed
+    error: "/auth/error", // Error page
+  },
 });
 
 export { handler as GET, handler as POST };
