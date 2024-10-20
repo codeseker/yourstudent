80e16071f3ca0f9679aa84428d716485e0b7b027
Ayush Jangid <ayushjangid441@gmail.com>
2024-10-20 23:09:37 +0530

    ts errors fixed

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -9,10 +9,6 @@ const handler = NextAuth({
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET, // Add this line
-  pages: {
-    signIn: "/auth/signin", // Custom sign-in page if needed
-    error: "/auth/error", // Error page
-  },
 });
 
 export { handler as GET, handler as POST };
