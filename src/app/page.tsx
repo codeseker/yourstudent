0375df259e10756d4fd43b5ff8231fc8ff7c2245
codeseker <ayushjangid441@gmail.com>
2025-04-21 00:17:10 +0530

    final push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
     };
 
     if (status === "authenticated" && session?.user?.email) {
-      // checking email 
+      // checking email authenticity
       checkValidUser();
     }
   }, [session, status]);
